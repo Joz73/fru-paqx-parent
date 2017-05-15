1faf46ec74a5845e81c0797c73afef5b9b90ab92
Joseph O'Leary <joseph.oleary@vce.com>
2017-05-15 09:34:38 +0100

    Adding test for checking cli version & setting target

--- test-packs/test_suites/fru_paqx_parent/fru_paqx_cli/test_fru_paqx_cli.py
+++ test-packs/test_suites/fru_paqx_parent/fru_paqx_cli/test_fru_paqx_cli.py
@@ -1,15 +1,17 @@
 import pytest
-import af_support_tools
 import traceback
 import requests
+import af_support_tools
 import executable_present
+import subprocess
+import platform
+import re
 
 try:
     env_file = 'env.ini'
     ipaddress = af_support_tools.get_config_file_property(config_file=env_file, heading='Base_OS', property='hostname')
     user = af_support_tools.get_config_file_property(config_file=env_file, heading='Base_OS', property='username')
     password = af_support_tools.get_config_file_property(config_file=env_file, heading='Base_OS', property='password')
-
 
 except Exception as e:
     print('Possible configuration error.')
@@ -20,36 +22,85 @@ except Exception as e:
 @pytest.mark.cli_chk
 def test_fru_cli_chk():
     # Arrange
-    file = "workflow-cli"
+    program = "workflow-cli"
 
     # Act/Assert
-    assert executable_present.executable_present(program=file), "CLI Not Present"
+    assert executable_present.executable_present(program=program), 'CLI Not Present'
 
 
 @pytest.mark.cli_version
 def test_fru_cli_version():
     # Arrange
-    command = './workflow-cli version'
+    os_system = platform.system()
+    print('\nMachine OS:' + os_system)
 
     # Act
-    response = af_support_tools.send_ssh_command(host=ipaddress,
-                                                 username=user,
-                                                 password=password,
-                                                 command=command,
-                                                 return_output=True)
+    try:
+        if os_system == 'Windows':
+            program = 'workflow-cli.exe'
+            command = [program, 'version']
+            inshell = True
+        else:
+            program = 'workflow-cli'
+            command = ['./'+program, 'version']
+            inshell = False
+
+        path = executable_present.executable_present(program=program)
+        response = subprocess.check_output(command, cwd=path, stderr=subprocess.STDOUT, shell=inshell)
+        response = response.decode('utf-8')
+        # Searches for version with pattern 'v[0-9].[0-9].[0-9]-[0-9][0-9]' eg.'v0.0.13-6'; else Returns None
+        searchObj = re.search(r'v\d\.\d\.\d-\d\d', response, re.M)
+        if searchObj:
+            version = searchObj.group()
+            print('Release Version: ' + version)
+
+    except Exception as err:
+        print('Unexpected error: ' + str(err))
+        traceback.print_exc()
+        raise Exception(err)
 
     # Assert
-    assert 'hi' in str(response)
+    assert searchObj is not None, 'Version Incorrect'
 
+
+@pytest.mark.cli_set_target
+def test_fru_cli_target():
+    # Arrange
+    os_system = platform.system()
+    print('\nMachine OS:' + os_system)
+
+    # Act
+    try:
+        if os_system == 'Windows':
+            program = 'workflow-cli.exe'
+            command = [program, 'target', 'https://{}:18443'.format(ipaddress)]
+            inshell = True
+        else:
+            program = 'workflow-cli'
+            command = ['./' + program, 'target', 'https://{}:18443'.format(ipaddress)]
+            inshell = False
+
+        path = executable_present.executable_present(program=program)
+        response = subprocess.check_output(command, cwd=path, stderr=subprocess.STDOUT, shell=inshell)
+        response = response.decode('utf-8')
+        print(response)
+
+    except Exception as err:
+        print('Unexpected error: ' + str(err))
+        traceback.print_exc()
+        raise Exception(err)
+
+    # Assert
+    assert 'Target set to https://{}:18443'.format(ipaddress) in response
 
 @pytest.mark.api
 def test_fru_api():
     # Arrange
-    url = 'https://{}:8443/fru/api/about'.format(ipaddress)
+    url = 'https://{}:18443/fru/api/about'.format(ipaddress)
 
     # Act
     response = requests.get(url, verify=False)
     print(url)
 
     # Assert
-    assert response.status_code == 200
+    assert response.status_code == 200, 'Unexpected API Response Code'
